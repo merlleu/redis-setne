817be5a2bb9324f6727bf7dda9e15e69900c6eb6
merlleu <r.langdorph@gmail.com>
2020-05-09 17:56:15 +0200

    changed reply "ok" to 1 or 0 (same as SETNX)

--- src/redis-setne.zig
+++ src/redis-setne.zig
@@ -22,13 +22,13 @@ export fn SETNE(ctx: ?*redis.RedisModuleCtx, argv: [*c]?*redis.RedisModuleString
         if (keyType == redis.REDISMODULE_KEYTYPE_STRING){
             var key_strlen: usize = undefined;
             var key_string = redis.RedisModule_StringDMA.?(key, &key_strlen, redis.REDISMODULE_READ)[0..key_strlen];
-            if (std.mem.eql(u8, val, key_string)) return redis.RedisModule_ReplyWithSimpleString.?(ctx, c"OK");
-        }   
+            if (std.mem.eql(u8, val, key_string)) return redis.RedisModule_ReplyWithSimpleString.?(ctx, c"0");
+        }
     }
 
     // Set the string using the high-level API. All args get routed to the native SET command.
     var reply = redis.RedisModule_Call.?(ctx, c"SET", c"!v", &(argv[1]), argc - 1);
-    return redis.RedisModule_ReplyWithCallReply.?(ctx, reply);
+    return redis.RedisModule_ReplyWithSimpleString.?(ctx, c"1");
 }
 
 
@@ -36,9 +36,9 @@ export fn RedisModule_OnLoad(ctx: *redis.RedisModuleCtx, argv: [*c]*redis.RedisM
     if (redis.RedisModule_Init(ctx, c"kristoff-it/setne", 1, redis.REDISMODULE_APIVER_1) == redis.REDISMODULE_ERR) {
         return redis.REDISMODULE_ERR;
     }
-    
+
     const err = redis.RedisModule_CreateCommand.?(ctx, c"setne", SETNE, c"write fast deny-oom", 1, 1, 1);
     if (err == redis.REDISMODULE_ERR) return redis.REDISMODULE_ERR;
-    
+
     return redis.REDISMODULE_OK;
 }
